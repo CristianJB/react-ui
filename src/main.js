2cf2f4e17d91f85c6ca7c8eab981062c8abc0256
barriquello <barriquello@gmail.com>
2016-08-02 16:32:51 -0300

    first app organization using material-ui

--- src/main.js
+++ src/main.js
@@ -10,20 +10,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Main from './app/Main'; // Our custom react component
+
 // Routes
-import Routes from './common/components/Routes';
+//import Routes from './common/components/Routes';
 
 // Base styling
 import './common/base.css';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 // ID of the DOM element to mount app on
 const DOM_APP_EL_ID = 'app';
 
 // Render the router
 ReactDOM.render((
-  <Router history={browserHistory}>
-    {Routes}
-  </Router>
+    <Main />
 ), document.getElementById(DOM_APP_EL_ID));
 
